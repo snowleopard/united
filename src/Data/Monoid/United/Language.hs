f622ce526a3986a06bcb7c0060f016d60eaa3db0
Andrey Mokhov <andrey.mokhov@gmail.com>
2020-02-15 00:15:48 +0000

    Add a monstrous generic fold

--- src/Data/Monoid/United/Language.hs
+++ src/Data/Monoid/United/Language.hs
@@ -2,13 +2,57 @@
 module Data.Monoid.United.Language where
 
 import Data.Array
+import Data.Bifunctor
 import Data.Monoid
 
 data U e a where
     Nil :: U e a
     Tip :: a -> U e a
     Bin :: e -> U e a -> U e a -> U e a
-    Let :: Ix i => Array i (U e a) -> U e (Either i a) -> U e a
+    Let :: (Ix i, Show i) => Array i (U e a) -> U e (Either i a) -> U e a
+
+fold :: forall a b e f. Applicative f => (forall x. f x)
+     -> (a -> f b)
+     -> (forall x. e -> f x -> f x -> f x)
+     -> (forall x i. (Ix i, Show i) => Array i (f x) -> f (Either i x) -> f x)
+     -> U e a
+     -> f b
+fold e t b l = go t
+  where
+    go :: (x -> f y) -> U e x -> f y
+    go t x = case x of
+      Nil       -> e
+      Tip a     -> t a
+      Bin e x y -> b e (go t x) (go t y)
+      Let arr x -> l (fmap (go t) arr) (go (either (pure . Left) (fmap (fmap Right) t)) x)
+
+instance Functor (U e) where
+    fmap f = fold Nil (Tip . f) Bin Let
+
+instance Applicative (U e) where
+    pure = Tip
+    f <*> x = fold Nil (\w -> fold Nil (Tip . w) Bin Let x) Bin Let f
+
+instance Bifunctor U where
+    bimap f g = fold Nil (Tip . g) (Bin . f) Let
+
+instance (Show e, Show a) => Show (U e a) where
+    show x = case x of
+        Nil -> "Nil"
+        Tip x -> "(Tip " ++ show x ++ ")"
+        Bin e x y -> "(Bin " ++ show e ++ " " ++ show x ++ " " ++ show y ++ ")"
+        Let arr x -> "(Let (" ++ show arr ++ ") " ++ show x ++ ")"
+
+-- Fold with caching of Let subexpressions
+foldc :: r -> (a -> r) -> (e -> r -> r -> r) -> U e a -> r
+foldc e t b = go
+  where
+    go x = case x of
+      Nil       -> e
+      Tip a     -> t a
+      Bin e x y -> b e (go x) (go y)
+      Let arr x -> let cache = fmap go arr
+                   in foldc e (either ((!) cache) t) b x
 
 type Set a = U () a
 type Graph a = U Any a
@@ -28,18 +72,8 @@ overlay = Bin mempty
 connect :: e -> U e a -> U e a -> U e a
 connect = Bin
 
-fold :: r -> (a -> r) -> (e -> r -> r -> r) -> U e a -> r
-fold e t b = go
-  where
-    go x = case x of
-      Nil       -> e
-      Tip a     -> t a
-      Bin e x y -> b e (go x) (go y)
-      Let arr x -> let cache = fmap go arr
-                   in fold e (either ((!) cache) t) b x
-
 toSet :: U e a -> Set a
-toSet = fold Nil Tip (const (Bin ()))
+toSet = first (const ())
 
 -- compose :: U e a -> U e a -> U e a
 
